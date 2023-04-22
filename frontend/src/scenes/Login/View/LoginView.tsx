45c8662f15f8e9685a37306b4d245826ff0d7b33
Davi Moreira <davimoreirafinanceiro@gmail.com>
2023-04-22 15:50:50 -0300

    brincando com o chakra ui

--- frontend/src/scenes/Login/View/LoginView.tsx
+++ frontend/src/scenes/Login/View/LoginView.tsx
@@ -1,18 +1,20 @@
 import React from "react"
-import { Button, ButtonGroup } from '@chakra-ui/react'
-import { Stack, HStack, VStack } from '@chakra-ui/react'
-import { Center, Square, Circle } from '@chakra-ui/react'
+import { Button } from '@chakra-ui/react'
+import { Stack } from '@chakra-ui/react'
 import "./Login.css"
 
 export default function LoginView(){
     return(
-        // <Center bg='tomato' h='auto' w={"max-content"} color='white'>
-        <div style={{margin:"auto"}}>
-        <Stack spacing={10} direction='column' align='center'>
-            <Button colorScheme='teal' size='lg'>LOGIN COM 42</Button>
-            <Button colorScheme='teal' size='lg'>LOGIN TESTE</Button>
-        </Stack>
-        </div>
-        // </Center>
+        <React.Fragment>
+            <h1 style={{textAlign:"center", fontWeight:"bold",fontSize:"70px", marginTop:"100px"}}>
+                TRANSPONG
+            </h1>
+            <div style={{margin:"auto"}}>
+                <Stack spacing={10} direction='column' align='center'>
+                    <Button colorScheme='teal' size='lg'>LOGIN COM 42</Button>
+                    <Button colorScheme='teal' size='lg'>LOGIN TESTE</Button>
+                </Stack>
+            </div>
+        </React.Fragment>
     )
 }
