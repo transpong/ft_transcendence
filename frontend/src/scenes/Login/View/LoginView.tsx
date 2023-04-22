194650287e61beebf261ca7ddd6c6c5c53d71d94
Davi Moreira <davimoreirafinanceiro@gmail.com>
2023-04-22 03:56:56 -0300

    brancando com o chakra-ui

---
+++ frontend/src/scenes/Login/View/LoginView.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Stack, HStack, VStack } from '@chakra-ui/react'
+import { Center, Square, Circle } from '@chakra-ui/react'
+
+export default function LoginView(){
+    return(
+        // <Center bg='tomato' h='auto' w={"max-content"} color='white'>
+        <div style={{width:"1000px"}}>
+        <Stack spacing={10} direction='column' align='center'>
+            <Button colorScheme='teal' size='lg'>LOGIN COM 42</Button>
+            <Button colorScheme='teal' size='lg'>LOGIN TESTE</Button>
+        </Stack>
+        </div>
+        // </Center>
+    )
+}
