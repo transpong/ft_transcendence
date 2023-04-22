38528b502b4369dffb804015398ac87452bd98df
Davi Moreira <davimoreirafinanceiro@gmail.com>
2023-04-22 04:20:52 -0300

    teste de css

--- frontend/src/scenes/Login/View/LoginView.tsx
+++ frontend/src/scenes/Login/View/LoginView.tsx
@@ -2,11 +2,12 @@ import React from "react"
 import { Button, ButtonGroup } from '@chakra-ui/react'
 import { Stack, HStack, VStack } from '@chakra-ui/react'
 import { Center, Square, Circle } from '@chakra-ui/react'
+import "./Login.css"
 
 export default function LoginView(){
     return(
         // <Center bg='tomato' h='auto' w={"max-content"} color='white'>
-        <div style={{margin:"auto", background:"red"}}>
+        <div style={{margin:"auto"}}>
         <Stack spacing={10} direction='column' align='center'>
             <Button colorScheme='teal' size='lg'>LOGIN COM 42</Button>
             <Button colorScheme='teal' size='lg'>LOGIN TESTE</Button>
