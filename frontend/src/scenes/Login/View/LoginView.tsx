e208da1287ebf57b7276efa24d491254c0b526e2
Davi Moreira <davimoreirafinanceiro@gmail.com>
2023-04-23 12:14:13 -0300

    retirando alterações inseridas para teste

--- frontend/src/scenes/Login/View/LoginView.tsx
+++ frontend/src/scenes/Login/View/LoginView.tsx
@@ -8,7 +8,7 @@ export default function LoginView(){
                 <figure className="gif">
                     <img className="school42" src="docs/gifs/42.gif" height="200px" width="200px"/>
                 </figure>
-                    <div style={{margin:"auto"}}>
+                    <div>
                     <Stack spacing={10} direction='column' align='center'>
                         <Button colorScheme='purple' size='lg'>LOGIN COM 42</Button>
                         <Button colorScheme='purple' size='lg'>LOGIN TESTE</Button>
