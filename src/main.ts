9b24e7ec0cfdcce76c6c38126d8db432fa22d288
Anderson Porto <anderson.higo2@gmail.com>
2023-04-24 21:54:48 -0300

    refactor: implicity types

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { INestApplication } from '@nestjs/common';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+async function bootstrap(): Promise<void> {
+  const app: INestApplication = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT || 3000);
 }
 
