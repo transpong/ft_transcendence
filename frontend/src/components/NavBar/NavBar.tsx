0bb63575154d32407a06feb795b13468df0261dd
Lucas Yuri <lucasyuri56@hotmail.com>
2023-04-25 17:40:17 -0300

    create navbar component

---
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -0,0 +1,21 @@
+import { ButtonGroup, Button, Avatar, HStack} from "@chakra-ui/react";
+import { SettingsIcon } from "@chakra-ui/icons";
+import './NavBar.css'
+
+export default function NavBar() {
+  return (
+    <HStack bgColor="#000" h="65px" align="center" justify="space-between">
+      <Button marginLeft="20px" paddingLeft="70px" paddingRight="70px">
+        Transpong
+      </Button>
+      <ButtonGroup>
+        <Button>Jogar</Button>
+        <Button>Jogos ao vivo</Button>
+        <Button>Ranking</Button>
+      </ButtonGroup>
+      <Button leftIcon={<Avatar />} rightIcon={<SettingsIcon />} size="lg">
+        Nickname
+      </Button>
+    </HStack>
+  );
+}
