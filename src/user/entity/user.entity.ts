9b49f7738a276165829cbe119bb97fef96754a5c
Anderson Porto <anderson.higo2@gmail.com>
2023-04-24 21:24:13 -0300

    add: user entity

---
+++ src/user/entity/user.entity.ts
@@ -0,0 +1,29 @@
+import { Column, Entity, ManyToMany, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class UserEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  @ManyToMany(() => UserEntity)
+  friendId: number;
+
+  @Column()
+  nickname: string;
+
+  @Column()
+  avatar: string;
+
+  @Column()
+  mfatoken: string;
+
+  @Column()
+  mfaValidateAt: Date;
+
+  @Column()
+  status: number;
+
+  @Column()
+  ftId: string;
+}
