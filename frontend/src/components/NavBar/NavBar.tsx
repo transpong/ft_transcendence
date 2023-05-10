7316d7fcd2575f2af0255ceb9de63cc808887dc2
Davi Moreira <davimoreirafinanceiro@gmail.com>
2023-05-09 23:00:51 -0300

    inseri um botão de logoff na home

--- frontend/src/components/NavBar/NavBar.tsx
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -1,9 +1,11 @@
-import { ButtonGroup, Button, Avatar, HStack} from "@chakra-ui/react";
+import { ButtonGroup, Button, Avatar, HStack, Text} from "@chakra-ui/react";
 import { SettingsIcon } from "@chakra-ui/icons";
+import { IoExit } from "react-icons/io5";
 import { useNavigate } from "react-router";
 
 export default function NavBar() {
   const navigate = useNavigate();
+  const logoff = () => navigate("/");
 
   return (
     <HStack h="14%" align="center" justify="space-between">
@@ -27,15 +29,18 @@ export default function NavBar() {
           Ranking
         </Button>
       </ButtonGroup>
-      <Button
-        colorScheme={"purple"}
-        leftIcon={<Avatar />}
-        rightIcon={<SettingsIcon />}
-        size="lg"
-        onClick={() => navigate("/home/me")}
+      <HStack
+        borderRadius={"0.375rem"}
+        backgroundColor={"#805AD5"}
+        minWidth={"2.5rem"}
       >
-        Nickname
-      </Button>
+        <HStack marginRight={"1rem"} marginLeft={"1rem"}>
+          <Avatar/>
+          <Text fontSize={"1rem"} fontWeight={"bold"} color={"white"}>Nickname</Text>
+          <SettingsIcon cursor={"pointer"} fontSize={"2rem"} fontWeight={"bold"} color={"white"} onClick={() => navigate("/home/me")}/>
+          <IoExit cursor={"pointer"} fontSize={"2rem"} fontWeight={"bold"} color={"white"} onClick={() => logoff()}/>
+        </HStack>
+      </HStack>
     </HStack>
   );
 }
