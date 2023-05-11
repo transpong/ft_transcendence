94c819f364a7ab6aad16112626ee34c7ed410004
Anderson Porto <anderson.higo2@gmail.com>
2023-05-11 15:51:48 -0300

    add: mfa generate and validate routes

--- src/user/controller/user.controller.ts
+++ src/user/controller/user.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Req } from '@nestjs/common';
+import { UserService } from '../service/user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post('me/mfa')
+  async generateMfaSecret(@Req() req) {
+    return this.userService.generateMfaSecret(req.user.ft_id);
+  }
+
+  @Post('me/mfa/validate')
+  async validateMfaSecret(@Req() req, @Body() body) {
+    return this.userService.validateMfaSecret(req.user.ft_id, body.mfa_token);
+  }
+}
--- src/user/service/user.service.ts
+++ src/user/service/user.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserEntity } from '../entity/user.entity';
 import { AuthDto } from '../../auth/dto/auth.dto';
+import { generateSecret, verify } from '2fa-util';
 
 @Injectable()
 export class UserService {
@@ -13,30 +14,60 @@ export class UserService {
 
   async createUser(authDto: AuthDto): Promise<void> {
     const user: UserEntity = AuthDto.toUserEntity(authDto);
-    user.created_at = new Date();
-    user.updated_at = new Date();
+    user.createdAt = new Date();
+    user.updatedAt = new Date();
 
     await this.userRepository.save(user);
-    console.log(`User ${user.ft_id} created!`); // TODO: remove this debug log
+    console.log(`User ${user.ftId} created!`); // TODO: remove this debug log
   }
 
-  async userExists(ft_id: string): Promise<boolean> {
+  async userExists(ftId: string): Promise<boolean> {
     const user: UserEntity = await this.userRepository.findOneBy({
-      ft_id: ft_id,
+      ftId: ftId,
     });
 
     return !!user;
   }
 
-  async userEmptyNickname(ft_id: string): Promise<boolean> {
+  async userEmptyNickname(ftId: string): Promise<boolean> {
     const user: UserEntity = await this.userRepository.findOne({
-      where: { ft_id },
+      where: { ftId },
     });
     if (!user) return false;
     return user.nickname === '';
   }
 
-  async getUserByFtId(ft_id: string): Promise<UserEntity> {
-    return await this.userRepository.findOneBy({ ft_id });
+  async getUserByFtId(ftId: string): Promise<UserEntity> {
+    const userEntity: UserEntity = await this.userRepository.findOneBy({
+      ftId,
+    });
+    if (!userEntity) {
+      throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+    }
+
+    return userEntity;
+  }
+
+  async generateMfaSecret(ftId: string) {
+    const userEntity: UserEntity = await this.getUserByFtId(ftId);
+    const mfaSecret = await generateSecret(userEntity.ftId, 'Transpong');
+
+    userEntity.tokenMFA = mfaSecret.secret;
+    await this.userRepository.update(userEntity.id, userEntity);
+    return {
+      secret: mfaSecret.secret,
+      qr_code_url: mfaSecret.qrcode,
+    };
+  }
+
+  async validateMfaSecret(ftId: string, token: string): Promise<void> {
+    const userEntity: UserEntity = await this.getUserByFtId(ftId);
+
+    if (await verify(userEntity.tokenMFA, token)) {
+      throw new HttpException('Invalid token', HttpStatus.BAD_REQUEST);
+    }
+
+    userEntity.validatedAtMFA = new Date();
+    await this.userRepository.update(userEntity.id, userEntity);
   }
 }
