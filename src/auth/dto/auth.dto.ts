fa108aac634e7c5e895a116a3cf19c167e460790
Anderson Porto <anderson.higo2@gmail.com>
2023-05-12 03:29:40 -0300

    add: mfa basic struct

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -3,15 +3,18 @@ import { UserEntity } from '../../user/entity/user.entity';
 export class AuthDto {
   username: string;
   image: string;
+  hasTwoFactor: boolean;
 
-  constructor(username: string, image: string) {
+  constructor(username: string, image: string, hasTwoFactor: boolean) {
     this.username = username;
     this.image = image;
+    this.hasTwoFactor = hasTwoFactor;
   }
 
   static fromJSON(json: any): AuthDto {
-    if (json.login === undefined) return new AuthDto(json.username, json.image);
-    return new AuthDto(json.login, json.image.link);
+    if (json.login === undefined)
+      return new AuthDto(json.username, json.image, json.mfa);
+    return new AuthDto(json.login, json.image.link, json.mfa);
   }
 
   static toUserEntity(authDto: AuthDto): UserEntity {
