a386d851790ca52b0d603b5a142c079eabf598a2
Anderson Porto <anderson.higo2@gmail.com>
2023-05-12 22:16:33 -0300

    add: update user entity

--- src/user/entity/user.entity.ts
+++ src/user/entity/user.entity.ts
@@ -87,4 +87,8 @@ export class UserEntity {
   twoFactorValid(): boolean {
     return !(this.tokenMFA !== null && this.validatedAtMFA === null);
   }
+
+  update(): void {
+    this.updatedAt = new Date();
+  }
 }
