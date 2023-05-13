39d735deb1875d9762dcbfa97dceb170c790d00a
Anderson Porto <anderson.higo2@gmail.com>
2023-05-13 02:06:12 -0300

    refactor: validation is string is integer

--- src/chat/dto/channel-input.dto.ts
+++ src/chat/dto/channel-input.dto.ts
@@ -1,12 +1,15 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsInt, IsNotEmpty, IsString } from 'class-validator';
 
 export class ChannelInputDto {
   @IsNotEmpty()
+  @IsString()
   name: string;
 
   @IsNotEmpty()
+  @IsString()
   password: string;
 
   @IsNotEmpty()
+  @IsInt()
   type: number;
 }
