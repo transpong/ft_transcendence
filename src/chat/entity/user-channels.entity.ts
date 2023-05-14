b5964da284539f0663bea174959375c41e128258
Anderson Porto <anderson.higo2@gmail.com>
2023-05-14 03:51:29 -0300

    add: changeUserRestrictions updateRestriction function

--- src/chat/entity/user-channels.entity.ts
+++ src/chat/entity/user-channels.entity.ts
@@ -8,6 +8,7 @@ import {
 } from 'typeorm';
 import { UserEntity } from '../../user/entity/user.entity';
 import { ChannelEntity } from './channel.entity';
+import { UserAccessType } from '../enum/access-type.enum';
 
 @Entity({ name: 'user_channels' })
 export class UsersChannelsEntity {
@@ -37,4 +38,33 @@ export class UsersChannelsEntity {
 
   @ManyToOne(() => ChannelEntity, (channel) => channel.users_channels)
   channel: ChannelEntity;
+
+  updateRestriction(restriction: string): void {
+    switch (restriction) {
+      case 'block':
+        this.userAccessType = UserAccessType.BLOCKED;
+        break;
+      case 'unblock':
+        this.userAccessType = UserAccessType.MEMBER;
+        break;
+      case 'kick':
+        this.kickedAt = new Date();
+        this.userAccessType = UserAccessType.KICKED;
+        break;
+      case 'unkick':
+        this.kickedAt = null;
+        this.userAccessType = UserAccessType.MEMBER;
+        break;
+      case 'mute':
+        this.mutedUntil = new Date(Date.now() + 1000 * 60 * 60 * 24);
+        this.userAccessType = UserAccessType.MUTED;
+        break;
+      case 'unmute':
+        this.mutedUntil = null;
+        this.userAccessType = UserAccessType.MEMBER;
+        break;
+      default:
+        break;
+    }
+  }
 }
