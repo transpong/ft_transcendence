ac68dd1ea5d6aebaacaf2452db425ba3a2d57ee8
Anderson Porto <anderson.higo2@gmail.com>
2023-05-15 03:03:29 -0300

    add: getMatchesHistory related functions

--- src/game/controller/game.controller.ts
+++ src/game/controller/game.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { GameService } from '../service/game.service';
+import { MatchesHistoryDto } from '../dto/matches-history.dto';
 
 @Controller('game')
-export class GameController {}
+export class GameController {
+  constructor(private readonly gameService: GameService) {}
+
+  @Get('matches-history')
+  getMatchesHistory(): Promise<MatchesHistoryDto[]> {
+    return this.gameService.getMatchesHistory();
+  }
+}
--- src/game/service/game.service.ts
+++ src/game/service/game.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { MatchHistoryEntity } from '../entity/game.entity';
+import { Repository } from 'typeorm';
+import { UserService } from '../../user/service/user.service';
+import { MatchesHistoryDto } from '../dto/matches-history.dto';
 
 @Injectable()
-export class GameService {}
+export class GameService {
+  @InjectRepository(MatchHistoryEntity)
+  private readonly matchHistoryRepository: Repository<MatchHistoryEntity>;
+  private readonly userService: UserService;
+
+  async getMatchesHistory(): Promise<MatchesHistoryDto[]> {
+    const matchesHistory: MatchHistoryEntity[] =
+      await this.getAllMatchesHistory();
+
+    return MatchesHistoryDto.toDtoList(matchesHistory);
+  }
+
+  private async getAllMatchesHistory() {
+    return this.matchHistoryRepository.find({
+      relations: ['user1', 'user2', 'winner'],
+      order: { createdAt: 'DESC' },
+    });
+  }
+}
