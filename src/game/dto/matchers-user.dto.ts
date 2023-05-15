e9137f378d2f2848ac0eb329b081c54a909c543a
Anderson Porto <anderson.higo2@gmail.com>
2023-05-15 03:03:36 -0300

    add: getMatchesHistory related dto

---
+++ src/game/dto/matchers-user.dto.ts
@@ -0,0 +1,45 @@
+import { Expose } from '@nestjs/class-transformer';
+import { MatchHistoryEntity } from '../entity/game.entity';
+import { UserEntity } from '../../user/entity/user.entity';
+
+export class MatchesUserDto {
+  @Expose({ name: 'id' })
+  id: number;
+
+  @Expose({ name: 'ft_id' })
+  ftId: string;
+
+  @Expose({ name: 'nickname' })
+  nickname: string;
+
+  @Expose({ name: 'is_winner' })
+  isWinner: boolean;
+
+  @Expose({ name: 'score' })
+  score: number;
+
+  @Expose({ name: 'avatar' })
+  avatar: string;
+
+  @Expose({ name: 'custom' })
+  custom: number;
+
+  static toDto(
+    matchHistory: MatchHistoryEntity,
+    user: UserEntity,
+  ): MatchesUserDto {
+    const matchHistoryDto: MatchesUserDto = new MatchesUserDto();
+
+    matchHistoryDto.id = user.id;
+    matchHistoryDto.ftId = user.ftId;
+    matchHistoryDto.nickname = user.nickname;
+    matchHistoryDto.isWinner = matchHistory.winner.id === user.id;
+    matchHistoryDto.score =
+      matchHistory.winner.id === user.id
+        ? matchHistory.user1Score
+        : matchHistory.user2Score;
+    matchHistoryDto.avatar = user.avatar;
+    matchHistoryDto.custom = matchHistory.custom;
+    return matchHistoryDto;
+  }
+}
