9cedc16804c793c17503aba2f92304acf340f661
Anderson Porto <anderson.higo2@gmail.com>
2023-05-18 03:42:21 -0300

    add: matchmaking poc

--- src/gateway/gateway.module.ts
+++ src/gateway/gateway.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { GatewayService } from './service/gateway.service';
 import { JwtModule } from '@nestjs/jwt';
 import { UserModule } from '../user/user.module';
+import { RoomService } from './service/room.service';
 
 @Module({
   imports: [
@@ -11,6 +12,6 @@ import { UserModule } from '../user/user.module';
     }),
     UserModule,
   ],
-  providers: [GatewayService],
+  providers: [GatewayService, RoomService],
 })
 export class GatewayModule {}
--- src/gateway/service/gateway.service.ts
+++ src/gateway/service/gateway.service.ts
@@ -5,42 +5,63 @@ import {
 } from '@nestjs/websockets';
 import { Socket } from 'socket.io';
 import { UserService } from '../../user/service/user.service';
+import { RoomService } from './room.service';
 
 @WebSocketGateway()
 export class GatewayService {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly roomService: RoomService,
+  ) {}
 
   @WebSocketServer()
   server: any;
-  users = 0;
 
-  async handleConnection(client: Socket) {
-    this.users++;
+  private users: Array<string> = [];
 
-    // Notify connected clients of current users
-    this.server.emit('users', this.users);
-
-    client.broadcast.emit('chat', 'test');
-  }
+  async handleConnection(client: Socket) {}
 
   async handleDisconnect() {
-    // A client has disconnected
-    this.users--;
-
-    // Notify connected clients of current users
     this.server.emit('users', this.users);
   }
 
-  @SubscribeMessage('chat')
-  async onChat(client, message) {
-    client.broadcast.emit('chat', message);
-    console.log(message);
-    client.broadcast.emit('message sent', message);
-    this.server.emit('chat', message);
+  @SubscribeMessage('joinRoom')
+  async handleRoom(client: Socket) {
+    this.users.push(client.id);
+
+    if (this.users.length === 2) {
+      console.log('users', this.users);
+      // create a new room if it doesn't exist
+      const user1 = this.users[0];
+      const user2 = this.users[1];
+
+      const roomName = `${user1}-${user2}`;
+
+      // add users to the room
+      this.server.socketsJoin(roomName);
+
+      // send the room name to the client
+      this.server.to(roomName).emit('room', roomName);
+
+      // send messages to the room
+      this.sendMessagesToRoom(roomName, 'Partida Encontrada');
+
+      // filter users from the list
+      this.users = this.users.filter(
+        (user) => user !== user1 && user !== user2,
+      );
+      console.log('users', this.users);
+    }
   }
 
-  // send to all connected clients
-  async sendToAll(message: string) {
-    this.server.emit('chat', message);
+  private sendMessagesToRoom(room: string, message: string) {
+    let counter = 0;
+    const interval = setInterval(() => {
+      this.server.to(room).emit('message', message);
+      counter++;
+      if (counter === 3) {
+        clearInterval(interval);
+      }
+    }, 1000);
   }
 }
