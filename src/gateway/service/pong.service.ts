d8b027ef6944b8d1eee86db3da6a8f2d47f6c75b
GitFlavioBC <fbonini887@gmail.com>
2023-05-30 14:42:11 -0300

    updated pong services

--- src/gateway/service/pong.service.ts
+++ src/gateway/service/pong.service.ts
@@ -4,16 +4,16 @@ import { Injectable } from '@nestjs/common';
 export class PongService {
   private readonly canvasWidth = 800; // Width of the game canvas
   private readonly canvasHeight = 600; // Height of the game canvas
-  private readonly paddleWidth = 10; // Width of the paddles
-  private readonly paddleHeight = 80; // Height of the paddles
-  private readonly ballSize = 10; // Size of the ball
+  private readonly widthPlayer = 10; // Width of the paddles
+  private readonly heightPlayer = 80; // Height of the paddles
+  private readonly diameterBall= 10; // Size of the ball
   private readonly paddleSpeed = 10; // Speed at which the paddles move
-  private readonly ballSpeed = 5; // Speed at which the ball moves
+  private readonly ballSpeed = 3; // Speed at which the ball moves
 
-  private player1Y = this.canvasHeight / 2 - this.paddleHeight / 2; // Initial Y position of player 1's paddle
-  private player2Y = this.canvasHeight / 2 - this.paddleHeight / 2; // Initial Y position of player 2's paddle
-  private ballX = this.canvasWidth / 2 - this.ballSize / 2; // Initial X position of the ball
-  private ballY = this.canvasHeight / 2 - this.ballSize / 2; // Initial Y position of the ball
+  private player1Y = this.canvasHeight / 2 - this.heightPlayer / 2; // Initial Y position of player 1's paddle
+  private player2Y = this.canvasHeight / 2 - this.heightPlayer / 2; // Initial Y position of player 2's paddle
+  private ballX = this.canvasWidth / 2 - this.diameterBall/ 2; // Initial X position of the ball
+  private ballY = this.canvasHeight / 2 - this.diameterBall/ 2; // Initial Y position of the ball
   private ballSpeedX = this.ballSpeed; // Initial speed of the ball along the X-axis
   private ballSpeedY = this.ballSpeed; // Initial speed of the ball along the Y-axis
 
@@ -34,7 +34,7 @@ export class PongService {
   }
 
   movePlayer1Down(): void {
-    if (this.player1Y + this.paddleHeight + this.paddleSpeed <= this.canvasHeight) {
+    if (this.player1Y + this.heightPlayer + this.paddleSpeed <= this.canvasHeight) {
       this.player1Y += this.paddleSpeed;
     }
   }
@@ -46,7 +46,7 @@ export class PongService {
   }
 
   movePlayer2Down(): void {
-    if (this.player2Y + this.paddleHeight + this.paddleSpeed <= this.canvasHeight) {
+    if (this.player2Y + this.heightPlayer + this.paddleSpeed <= this.canvasHeight) {
       this.player2Y += this.paddleSpeed;
     }
   }
@@ -56,18 +56,18 @@ export class PongService {
     this.ballY += this.ballSpeedY;
 
     // Check collision with walls
-    if (this.ballY <= 0 || this.ballY + this.ballSize >= this.canvasHeight) {
+    if (this.ballY <= 0 || this.ballY + this.diameterBall>= this.canvasHeight) {
       this.ballSpeedY *= -1; // Reverse the ball's Y speed
     }
 
     // Check collision with paddles
     if (
-      (this.ballX <= this.paddleWidth &&
-        this.ballY + this.ballSize >= this.player1Y &&
-        this.ballY <= this.player1Y + this.paddleHeight) ||
-      (this.ballX + this.ballSize >= this.canvasWidth - this.paddleWidth &&
-        this.ballY + this.ballSize >= this.player2Y &&
-        this.ballY <= this.player2Y + this.paddleHeight)
+      (this.ballX <= this.widthPlayer &&
+        this.ballY + this.diameterBall>= this.player1Y &&
+        this.ballY <= this.player1Y + this.heightPlayer) ||
+      (this.ballX + this.diameterBall>= this.canvasWidth - this.widthPlayer &&
+        this.ballY + this.diameterBall>= this.player2Y &&
+        this.ballY <= this.player2Y + this.heightPlayer)
     ) {
       this.ballSpeedX *= -1; // Reverse the ball's X speed
     }
@@ -87,8 +87,8 @@ export class PongService {
   }
 
   resetBallPosition(scoringPlayer: number): void {
-    this.ballX = this.canvasWidth / 2 - this.ballSize / 2; // Reset X position of the ball
-    this.ballY = this.canvasHeight / 2 - this.ballSize / 2; // Reset Y position of the ball
+    this.ballX = this.canvasWidth / 2 - this.diameterBall/ 2; // Reset X position of the ball
+    this.ballY = this.canvasHeight / 2 - this.diameterBall/ 2; // Reset Y position of the ball
 
     // Reset X speed of the ball based on the scoring player
     this.ballSpeedX = this.ballSpeed * (scoringPlayer === 1 ? 1 : -1);
@@ -103,6 +103,7 @@ export class PongService {
       this.serverTmp = server;
       // Start the game loop
       this.gameLoopInterval = setInterval(() => {
+        // if conexao off emit endGame
         this.updateGameState();
         const gameState = this.getGameState();
         this.serverTmp.to(this.roomNameTmp).emit('pong', gameState);
@@ -129,6 +130,8 @@ export class PongService {
 
     // Check if the game is over
     if (this.timer <= 0) {
+      const gameState = this.getGameState();
+      this.serverTmp.to(this.roomNameTmp).emit('pong', gameState);
       this.stopGameLoop();
     }
   }
@@ -161,8 +164,11 @@ export class PongService {
     return {
       player1Y: this.player1Y,
       player2Y: this.player2Y,
+      heightPlayer: this.heightPlayer,
+      widthPlayer: this.widthPlayer,
       ballX: this.ballX,
       ballY: this.ballY,
+      diameterBall: this.diameterBall,
       player1Score: this.player1Score,
       player2Score: this.player2Score,
       timer: this.timer,
