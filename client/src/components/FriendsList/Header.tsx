443d1a0cd1aa8cb2d2b8864629438a755699a5ef
Anderson Porto <anderson.higo2@gmail.com>
2023-06-05 22:41:35 -0300

    refactor: header colors

--- client/src/components/FriendsList/Header.tsx
+++ client/src/components/FriendsList/Header.tsx
@@ -1,28 +1,39 @@
-import {MinusIcon, AddIcon } from "@chakra-ui/icons";
-import { Flex, Text } from "@chakra-ui/react";
+import { MinusIcon, AddIcon } from "@chakra-ui/icons";
+import { Flex, Text, useColorModeValue } from "@chakra-ui/react";
 
 type Props = {
-  minimized: () => void;
-  getStatusMinimized: () => boolean;
-} 
+    minimized: () => void;
+    getStatusMinimized: () => boolean;
+};
 
 const Header = (props: Props) => {
-  return (
-    <Flex minHeight={"60px"} backgroundColor={"#805AD5"} borderTopRadius={"20px"} justify={"center"} cursor={"pointer"} onClick={() => props.minimized()}>
-      <Flex flexDirection="column" mx="5" justify="center">
-        <Text color={"white"} fontSize="larger" fontWeight="bold">
-          CHAT
-        </Text>
-      </Flex>
-      <Flex align={"center"} flexDirection={"row-reverse"}>
-      {
-          (props.getStatusMinimized() ? 
-          <MinusIcon/> : 
-          <AddIcon/> )
-      }
-      </Flex>
-    </Flex>
-  );
+    return (
+        <Flex
+            minHeight={"40px"}
+            backgroundColor={"#805AD5"}
+            borderTopRadius={""}
+            justify={"center"}
+            cursor={"pointer"}
+            onClick={() => props.minimized()}
+            _hover={{
+                bg: useColorModeValue("#402d6a", "#402d6a"),
+            }}
+        >
+            <Flex flexDirection="column" mx="5" justify="center">
+                <Text
+                    color={useColorModeValue("#805AD5", "#805AD5")}
+                    fontSize="larger"
+                    fontWeight="bold"
+                    style={{ color: "white" }} // Adicionado estilo inline para definir a cor roxa
+                >
+                    CHAT
+                </Text>
+            </Flex>
+            <Flex align={"center"} flexDirection={"row-reverse"}>
+                {props.getStatusMinimized() ? <MinusIcon /> : <AddIcon />}
+            </Flex>
+        </Flex>
+    );
 };
 
 export default Header;
