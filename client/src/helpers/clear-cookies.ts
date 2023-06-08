e4c27152cdbaceb18ae25211d91bd430d94cf868
Anderson Porto <anderson.higo2@gmail.com>
2023-06-08 16:44:59 -0300

    Fix cookie samesite (#30)
    * fix: cookie missing same site

--- client/src/helpers/clear-cookies.ts
+++ client/src/helpers/clear-cookies.ts
@@ -1,6 +1,6 @@
 export function clearCookies () {
   const cookies = document.cookie.split(";");
   for (let i = 0; i < cookies.length; i++) {
-    document.cookie = cookies[i] + "=; expires=" + new Date(0).toUTCString();
+    document.cookie = cookies[i] + "=; expires=" + new Date(0).toUTCString() + "; SameSite=None; secure";
   }
 }
